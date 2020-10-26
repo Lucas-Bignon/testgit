2de057946cbd437d71c72ab6be7ee66929d05cce
romgrm <romaingreaume@gmail.com>
2020-10-26 13:33:16 +0100

    Save

---
+++ exo1/.history/src/main/java/co/testgit/App_20201026132659.java
@@ -0,0 +1,43 @@
+package co.testgit;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+		throw new IllegalStateException("Test class");
+	}
+    public static String helloBoyAndGirl(String name){
+        return "Hello World";
+    }
+    public static String helloLucas(String name){
+        return "Hello Lucas";
+    }
+
+
+    public static String helloRomain(String name){
+        
+        String helloPrenom = "Hello Romain"; 
+        if(name == ""){
+            name = helloPrenom;  
+        }
+        return name; 
+    }
+
+    public static String helloAmin(String name){
+        
+        String helloPrenom = "Hello Amin"; 
+        if(name == ""){
+            name = helloPrenom;  
+        }
+        return name; 
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
